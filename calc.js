1225bd335dbbccb6ced6666ec851e0d3b2074b3c
Riza Fahmi <rizafahmi@gmail.com>
2019-02-06 16:22:41 +0700

    Remove print feature

--- calc.js
+++ calc.js
@@ -9,7 +9,6 @@ const add = (a, b) => {
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
-const print = (text) => console.log(text);
 
 module.exports = {
     add, sub, mul, div
